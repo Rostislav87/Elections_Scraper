8efd63f2a07e6b5528ccd47a11c22f47a912db78
Rostislav Rabiec <rosta.rabiec@icloud.com>
2023-01-03 15:14:16 +0100

    Beta 0.1.0 Program is almost finished. The program works as it should. Downloading datas, writing and saving as CSV file. Missing only start program with arguments. Under construction.

--- elections.py
+++ elections.py
@@ -16,7 +16,7 @@ import csv
 # Ve výstupu (soubor .csv) každý řádek obsahuje informace pro konkrétní obec. Tedy podobu:
 # kód obce, název obce, voliči v seznamu, vydané obálky, platné hlasy, kandidující strany
 
-
+print("Stahuji data...")
 html = "https://www.volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=14&xnumnuts=8105" 
 soup = bs(requests.get(html).text, "html.parser")
 cell = soup.find_all("td")
@@ -29,51 +29,59 @@ list_towns = chart[1::3]
 
 town_codes = list_codes[:-1:] # Seznam kódů obcí
 town_names = list_towns[:-1:] # Seznam názvů obcí
-list_of_voters = list() # Seznam voličů 
-list_of_envelopes = list() # Seznam vydaných obálek
-list_of_valid_votes = list() # Seznam platných hlasů
+data1 = list() # Seznam voličů
+data2 = list() # Seznam vydaných obálek
+data3 = list() # Seznam platných hlasů
+parties = list() # Seznam všech politických strana
+party_votes = list() # Seznam všech hlasů za každou obec
 
 for code in town_codes:   
     town_url = f"https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=14&xobec={code}&xvyber=8105"
     soup2 = bs(requests.get(town_url).text, "html.parser")  
-
+  
     voters = soup2.find_all("td", {"headers": "sa2"}) # Voliči v seznamu
     for votes in voters:
-        list_of_voters.append(votes.text)
-
+        data1.append(votes.text.replace("\xa0", ""))
+        
     envelopes = soup2.find_all("td", {"headers": "sa3"}) # Vydané obálky
     for envelope in envelopes:
-        list_of_envelopes.append(envelope.text)
-
+        data2.append(envelope.text.replace("\xa0", ""))
+    
     valid_votes = soup2.find_all("td", {"headers": "sa6"}) # Platné hlasy
     for votes in valid_votes:
-        list_of_valid_votes.append(votes.text)
+        data3.append(votes.text.replace("\xa0", ""))
 
+    parties_tab1 = soup2.find_all("td", {"headers": "t1sa1 t1sb2"}) # Politické strany (1. tabulka)
+    parties_tab2 = soup2.find_all("td", {"headers": "t2sa1 t2sb2"}) # Politické strany (2. tabulka)
+    parties_votes1 = soup2.find_all("td", {"headers": "t1sa2 t1sb3"}) # Platné hlasy pro každou stranu (1. tabulka)
+    parties_votes2 = soup2.find_all("td", {"headers": "t2sa2 t2sb3"}) # Platné hlasy pro každou stranu (2. tabulka)
+    parties = [party.text for party in parties_tab1] + [party.text for party in parties_tab2]
+    list_of_votes = list()
+    for vote in parties_votes1:
+        list_of_votes.append(vote.text.replace("\xa0", ""))
+    for vote in parties_votes2:
+        list_of_votes.append(vote.text.replace("\xa0", ""))
+    party_votes.append(list_of_votes)
 
-    parties_votes1 = soup2.find_all("td", {"headers": "t1sa2 t1sb3"}) # Platné hlasy pro kažou pol. stranu 1
-    parties_votes2 = soup2.find_all("td", {"headers": "t2sa2 t2sb3"}) # Platné hlasy pro kažou pol. stranu 2
-    
-       
+list_of_votes_sorted_by_parties = [[row[i] for row in party_votes] for i in range(26)]
 
-# CSV zápis
-parties_url = "https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=14"
-soup3 = bs(requests.get(parties_url).text, "html.parser")
-parties1 = soup3.find_all("td", {"headers": "t1sa1 t1sb2"}) # Politické strany 1 
-parties2 = soup3.find_all("td", {"headers": "t2sa1 t2sb2"}) # Politické strany 2
+dict1 = {
+    "Kód obce": town_codes,
+    "Název obce": town_names,
+    "Voliči v seznamu": data1,
+    "Vydané obálky": data2,
+    "Platné hlasy": data3
+    }
+dict2 = dict()
+for key, item in zip(parties, list_of_votes_sorted_by_parties):
+    dict2[key] = item
 
-header = ["Kód obce", "Název obce", "Voliči v seznamu", "Vydané obálky", "Platné hlasy"]
-for party in parties1:
-    header.append(party.text)
-for party in parties2:
-    header.append(party.text)
+dict1.update(dict2)
 
-rows = zip(town_codes, town_names, list_of_voters, list_of_envelopes, list_of_valid_votes)
+print("Zapisuji data...")
+with open("results_opava.csv", mode="w", newline="") as new_file:
+    writer = csv.writer(new_file, delimiter=";")    
+    writer.writerow(dict1.keys())
+    writer.writerows(zip(*dict1.values()))
 
-# with open("results_opava.csv", mode="w", newline="") as new_file:
-#     writer = csv.writer(new_file, delimiter=";") 
-#     writer.writerow(header)
-#     for row in rows:
-#         writer.writerow(row)
-
-
-
+print("Hotovo.")
